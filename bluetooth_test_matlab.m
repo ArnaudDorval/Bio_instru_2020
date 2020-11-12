fe222985f8971720bc762be56dd3276a2c4cdec3
ArnaudDorval <arnaud.dorval-leblanc.1@ulaval.ca>
2020-11-12 12:03:46 -0500

    First commit du script d'acquisition de données sur matlab

---
+++ bluetooth_test_matlab.m
@@ -0,0 +1,30 @@
+%"HC-06"--"98D332202FDA"--1--"Requires pairing"
+%98D332202FDA
+
+%bluetoothlist;
+
+myDataSet = [0 0];
+RawData = "rawData";
+
+hc06 = bluetooth("HC-06", 1);
+write(hc06, "init");
+a = read(hc06, 2, "string");
+disp(a);
+for index = 1:10
+   write(hc06, "b");
+   a = read(hc06, 9, "string");
+   RawData = [RawData; a];
+end
+disp(RawData);
+
+%faut je parse le data
+for i = RawData
+   formatted = uint16( str2double( regexp( a, '\;', 'split' ) ) )
+   myDataSet = [myDataSet; formatted];
+end
+disp(myDataSet);
+clear hc06;
+
+
+
+
