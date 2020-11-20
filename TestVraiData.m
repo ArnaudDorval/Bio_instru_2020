386c4c9b34b82155942f33cd0fc26eccc2a0071f
Etienne Ransford <etienne.ransford.1@ulaval.ca>
2020-11-20 13:02:13 -0500

    modifs

--- TestVraiData.m
+++ TestVraiData.m
@@ -1,6 +1,6 @@
 clear;
 clc;
-hold off
+% hold off
 hold on
 %% Chargement des données
 load('EtienneParsedData');
@@ -9,9 +9,21 @@ load('ArnaudParsedData');
 load('Dataraw1');
 
 
-Light = mean(ArnaudSetD(4:4:end,2));
-IR = (ArnaudSetD(1:4:end-2,2)-Light)*3.3/(2^16);
-R = (ArnaudSetD(3:4:end,2)-Light)*3.3/(2^16);
+Light = mean(EtienneSetA(4:4:end,2));
+IR = (EtienneSetA(1:4:end-2,2)-Light)*3.3/(2^16);
+R = (EtienneSetA(3:4:end,2)-Light)*3.3/(2^16);
+
+% Light = mean(DataSet1fknDown(4:4:end,1));
+% IR = (DataSet1fknDown(1:4:end-2,1)-Light)*3.3/(2^16);
+% R = (DataSet1fknDown(3:4:end,1)-Light)*3.3/(2^16);
+
+% Light = mean(ArnaudSetD(4:4:end,2));
+% IR = (ArnaudSetD(1:4:end-2,2)-Light)*3.3/(2^16);
+% R = (ArnaudSetD(3:4:end,2)-Light)*3.3/(2^16);
+
+% Light = mean(BenjaminSetD(4:4:end,2));
+% IR = (BenjaminSetD(1:4:end-2,2)-Light)*3.3/(2^16);
+% R = (BenjaminSetD(3:4:end,2)-Light)*3.3/(2^16);
 
 % plot(R)
 % plot(IR)
@@ -21,45 +33,43 @@ R = (ArnaudSetD(3:4:end,2)-Light)*3.3/(2^16);
 RDC = mean(R);
 IRDC = mean(IR);
 %% filtre PB
-% On veut enlever le gros bruit
-L = 10; %% Largeur de filtre, Plus L est grand plus le filtrage est intense
-f = 1/L*ones(L,1);
-R = conv(R,f);
-IR = conv(IR,f);
-R = R(L:length(R)-L);
-IR = IR(L:length(IR)-L);
+
+ParaPB = 0.03;
+
+[R,PB1] = lowpass(R,ParaPB);
+[IR,PB2] = lowpass(IR,ParaPB);
+
+
+R = R(length(PB1.Coefficients):length(R)-length(PB1.Coefficients));
+IR = IR(length(PB2.Coefficients):length(IR)-length(PB2.Coefficients));
 
 
 % plot(R)
 % plot(IR)
 
-%% Filtre passe haut
-% On veut enlever la composante DC
-ParaPH = 0.007;
-RAC = highpass(R,ParaPH);
-IRAC = highpass(IR,ParaPH);
+%% Composante AC
+
+RAC = -R;
+IRAC = -IR;
+
+RAC = RAC - min(RAC);
+IRAC = IRAC - min(IRAC);
 
 % plot(RAC)
 % plot(IRAC)
 
-%% Deuxieme filtre passe bas
+
+%% Filtre passe haut
+% On veut enlever la composante DC plus ParaPH est haut, plus la fréquence
+% de coupur est haute
+ParaPH = 0.01;
+[RAC, PH1] = highpass(RAC,ParaPH);
+[IRAC, PH2] = highpass(IRAC,ParaPH);
 
 
 
-L2 = 20; %% Largeur de filtre, Plus L est grand plus le filtrage est intense
-f2 = 1/L2*ones(L2,1);
-
-RAC= conv(RAC,f2);
-IRAC = conv(IRAC,f2);
-
-
-
-RAC = -RAC(L:length(RAC)-L);
-IRAC = -IRAC(L:length(IRAC)-L);
-
-RAC = RAC - mean(RAC);
-IRAC = IRAC - mean(IRAC);
-
+RAC = RAC(length(PH1.Coefficients):length(RAC)-length(PH1.Coefficients));
+IRAC = IRAC(length(PH2.Coefficients):length(IRAC)-length(PH2.Coefficients));
 
 plot(RAC)
 plot(IRAC)
@@ -69,6 +79,6 @@ legend('R','IR')
 
 
 %%
-SaO2 =110-25*((rms(RAC)/RDC)./(rms(IRAC)/IRDC));
-meanSaO2 = trimmean(SaO2,10) % trouve la moyenne des valeurs excluant les 20% des valeurs
+SaO2 =110-25*(((RAC)/RDC)./((IRAC)/IRDC));
+meanSaO2 = trimmean(SaO2,5) % trouve la moyenne des valeurs excluant les 20% des valeurs
 %%les plus loin de la moyenne (données abérantes)
